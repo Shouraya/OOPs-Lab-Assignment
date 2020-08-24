56dca2b7e6ee8ce006568e5a7191e093ad5bc0ff
Shouraya <shourayagoyal2406@gmail.com>
2020-08-24 11:50:26 +0530

    blank commit

--- Lab Assignment 1/1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int num;
-    cout << "Enter an integer: ";
-    cin >> num;
-    num%2 == 0 ? cout << "The number is even" : cout<<"The number is odd";
-}
--- Lab Assignment 1/2b.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    for(int i=2; i<21; i+=2)
-    {
-        cout<<i<<" ";
-    }
-}
